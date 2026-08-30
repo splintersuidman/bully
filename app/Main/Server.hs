9d9123987bb681ca55610b407eda2fb99b2b5791
Splinter Suidman <splinter@mannenopdemaan.nl>
2026-08-30 17:38:27 +0200

    Update README.md

--- app/Main/Server.hs
+++ app/Main/Server.hs
@@ -3,5 +3,6 @@ module Main (main) where
 import Bully.Config qualified as Config
 import Bully.Server qualified as Server
 
+-- TODO: Parse config from command line arguments.
 main :: IO ()
 main = Server.serve Config.defaultServerConfig
